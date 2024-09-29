3cad730a32abaf555a71d36f4562a0b836dc9275
Xenon004 <134205072+Xenon004@users.noreply.github.com>
2024-09-29 15:13:18 +0530

    Updates

---
+++ precache-manifest.e9d21ea433b2f75f0e414dd31021868f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb3d191da5f006d37259b5fc9660555",
+    "url": "/textilis/index.html"
+  },
+  {
+    "revision": "761f2596bc78bd47778c",
+    "url": "/textilis/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "a4d6159609417f3380ec",
+    "url": "/textilis/static/js/2.a4b03213.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textilis/static/js/2.a4b03213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab8e91db0bf995ebef2",
+    "url": "/textilis/static/js/3.4d5280cd.chunk.js"
+  },
+  {
+    "revision": "761f2596bc78bd47778c",
+    "url": "/textilis/static/js/main.cb4cd26b.chunk.js"
+  },
+  {
+    "revision": "c1bc73690207babdf11f",
+    "url": "/textilis/static/js/runtime-main.1087d871.js"
+  }
+]);
